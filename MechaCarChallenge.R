5c7cfd2cfaf1bda7296bda3ea63e5cd194ad9467
dmoronfolu <85265504+dmoronfolu@users.noreply.github.com>
2021-10-02 12:31:54 -0500

    Adding MechaCarChallenge files

---
+++ MechaCarChallenge.R
@@ -0,0 +1,5 @@
+library(tidyverse)
+MechaCarData <- read.csv("MechaCar_mpg.csv")
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCarData)
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCarData))   
+        
